4d60a19309da8f1723b32e6c8cbc4a2ef83ed81e
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-09-25 09:07:49 -0300

    First Commit - Criando a base

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.safe}>
+      <NavigationContainer>
+      <StatusBar />
+
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/LastActivityScreen.js
@@ -0,0 +1,10 @@
+import { View, Text} from "react-native"
+
+export default function LastActivityScreen() {
+
+    return (
+        <View>
+            <Text>Não existem atividades recentes.</Text>
+        </View>
+    )
+}
