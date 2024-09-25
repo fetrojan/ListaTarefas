3cef57c4f95e97d72a548b0691148d94ba2089a9
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-09-25 09:29:43 -0300

    Criadas as screens para o app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import TasksScreen from './screens/TasksScreen'
+import LastActivityScreen from './screens/LastActivityScreen';
+import MessagesScreen from './screens/MessagesScreen';
 
 const Tab = createMaterialTopTabNavigator()
 
@@ -10,8 +13,14 @@ export default function App() {
       <NavigationContainer>
       <StatusBar />
 
-      <Text>Open up App.js to start working on your app!</Text>
-      
+      <Tab.Navigator initialRouteName='Tasks' screenOptions={{ tabBarIndicatorStyle: {backgroundColor: 'green'}}}>
+
+        <Tab.Screen name='Messages' component={MessagesScreen}/>
+        <Tab.Screen name="Tasks" component={TasksScreen} />
+        <Tab.Screen name="Last Activity" component={LastActivityScreen} />
+
+      </Tab.Navigator>
+
       </NavigationContainer>
     </SafeAreaView>
   );
---
+++ screens/TasksScreen.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+
+export default function TasksScreen() {
+
+    return (
+        <View>
+            <Text>Não existem mensagens recentes.</Text>
+        </View>
+    )
+}
